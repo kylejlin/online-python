5fa1ab8b40e19a2e6f102fa09429eafce7618dc9
Kyle Lin <kylejlin.us@gmail.com>
2024-07-22 15:51:01 -0700

    Add dummy stackframe.js file

---
+++ public/stackframe.js
@@ -0,0 +1,8 @@
+// This is just a dummy file to suppress an error.
+// For whatever reason, the Pyodide JS code attempts to
+// load a nonexistent `stackframe.js` file from the root directory.
+// This results in an annoying create-react-app error overlay
+// when in development mode.
+// To suppress this, I created this dummy file,
+// so that the request to load `stackframe.js` will succeed
+// (albeit do nothing).
