7d2f1ed3520713b6e483093f7c0543fd4c289f5f
Kyle Lin <kylejlin.us@gmail.com>
2024-07-22 11:55:36 -0700

    Add handleEditorChange

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,28 @@ import React from "react";
 import "./App.css";
 import Editor from "@monaco-editor/react";
 
+const DEFAULT_EDITOR_VALUE =
+  'x = int(input("Enter a number: "))\ny = int(input("Enter a second number: "))\nz = x + y\nprint(f"The sum of the two numbers is {z}")';
+
 interface AppProps {}
 
-interface AppState {}
+interface AppState {
+  readonly editorValue: string;
+}
 
 export class App extends React.Component<AppProps, AppState> {
   constructor(props: AppProps) {
     super(props);
-    this.state = {};
+
+    this.state = {
+      editorValue: DEFAULT_EDITOR_VALUE,
+    };
+
+    this.bindMethods();
+  }
+
+  bindMethods(): void {
+    this.handleEditorChange = this.handleEditorChange.bind(this);
   }
 
   render() {
@@ -18,9 +32,21 @@ export class App extends React.Component<AppProps, AppState> {
         <Editor
           height="90vh"
           defaultLanguage="python"
-          defaultValue="# Some comment"
+          defaultValue={DEFAULT_EDITOR_VALUE}
+          onChange={this.handleEditorChange}
         />
       </div>
     );
+  }
+
+  handleEditorChange(value: string | undefined): void {
+    if (value === undefined) {
+      return;
+    }
+
+    this.setState({
+      editorValue: value,
+    });
+    console.log(value);
   }
 }
